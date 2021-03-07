3357417be73050516b982841092da6bd1d61a050
Richard Caarels <rcaarels@gmail.com>
2021-03-07 21:39:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+//Message tonen
+function showMessage() {
+    document.getElementById("result").style.display = "block";
+};
+
+//Rente berekenen
+function compute() {
+    var principal = document.getElementById("principal").value;
+    if (parseInt(principal) < 1) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return;
+    }
+    //haalt waarden op
+
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    var interest = principal * years * rate / 100;
+
+    var today = new Date().getFullYear();
+    var yearOfWithdrawal = parseInt(today) + parseInt(years);
+
+    document.getElementById("depositmessage").innerHTML = "" + principal;
+    document.getElementById("interestmessage").innerHTML = "" + rate;
+    document.getElementById("amountmessage").innerHTML = "" + interest;
+    document.getElementById("yearmessage").innerHTML = "" + yearOfWithdrawal;
+    showMessage();
+
+    return false;
+};
+// slider range ophalen en wijzigen
+function showRange() {
+    var rate = document.getElementById("rate").value;
+    document.getElementById("ratevalue").innerHTML = rate;
 }
-        
